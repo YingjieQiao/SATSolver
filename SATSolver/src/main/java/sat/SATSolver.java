5d638160c91c06e015c334dd18562f5863c61490
PJX <pjingxiang@gmail.com>
2020-10-21 20:09:29 +0800

    Add files via upload

--- SATSolver/src/main/java/sat/SATSolver.java
+++ SATSolver/src/main/java/sat/SATSolver.java
@@ -1,10 +1,15 @@
 package sat;
 
+import java.util.Iterator;
+
+import immutable.EmptyImList;
 import immutable.ImList;
 import sat.env.Environment;
 import sat.formula.Clause;
 import sat.formula.Formula;
 import sat.formula.Literal;
+import sat.formula.PosLiteral;
+
 
 /**
  * A simple DPLL SAT solver. See http://en.wikipedia.org/wiki/DPLL_algorithm
@@ -15,19 +20,21 @@ public class SATSolver {
      * unit propagation. The returned environment binds literals of class
      * bool.Variable rather than the special literals used in clausification of
      * class clausal.Literal, so that clients can more readily use it.
-     * 
+     *
      * @return an environment for which the problem evaluates to Bool.TRUE, or
      *         null if no such environment exists.
      */
     public static Environment solve(Formula formula) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        ImList<Clause> res = formula.getClauses();
+        Environment res_env = new Environment();
+        return solve(res, res_env);
+
     }
 
     /**
      * Takes a partial assignment of variables to values, and recursively
      * searches for a complete satisfying assignment.
-     * 
+     *
      * @param clauses
      *            formula in conjunctive normal form
      * @param env
@@ -37,14 +44,88 @@ public class SATSolver {
      *         or null if no such environment exists.
      */
     private static Environment solve(ImList<Clause> clauses, Environment env) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+        if (clauses.isEmpty()){
+            return env;
+        }
+
+        Iterator<Clause> check_empty = clauses.iterator();
+        while (check_empty.hasNext()){
+            if (check_empty.next().isEmpty()){
+                return null;
+            }
+        }
+
+        int min = 9999;
+        Clause min_x = new Clause();
+        for (Clause x : clauses){
+            if (x.size() < min){
+                min = x.size();
+                min_x = x;
+            }
+        }
+
+        Environment env_true = new Environment();
+        Environment res = new Environment();
+
+        if (min_x.isUnit()){
+            if (min_x.chooseLiteral() instanceof PosLiteral){
+                env_true = env.putTrue(min_x.chooseLiteral().getVariable());
+                ImList<Clause> sub = new EmptyImList<Clause>();
+                sub = substitute(clauses, min_x.chooseLiteral());
+                res = solve(sub, env_true);
+            }
+            else{
+                env_true = env.putFalse(min_x.chooseLiteral().getVariable());
+                ImList<Clause> sub = new EmptyImList<Clause>();
+                sub = substitute(clauses, min_x.chooseLiteral());
+                res = solve(sub, env_true);
+            }
+
+
+        }else{
+            Literal lit = min_x.chooseLiteral();
+
+            if (lit instanceof PosLiteral){
+                env_true = env.putTrue(lit.getVariable());
+                ImList<Clause> sub = new EmptyImList<Clause>();
+                sub = substitute(clauses, lit);
+                res = solve(sub, env_true);
+            }else{
+                env_true = env.putFalse(lit.getVariable());
+                ImList<Clause> sub = new EmptyImList<Clause>();
+                sub = substitute(clauses,lit);
+                res = solve(sub, env_true);
+            }
+            if (res == null){
+                Literal new_lit = lit.getNegation();
+                if (new_lit instanceof PosLiteral){
+                    env_true = env.putTrue(lit.getVariable());
+                    ImList<Clause> sub = new EmptyImList<Clause>();
+                    sub = substitute(clauses, new_lit);
+                    res = solve(sub, env_true);
+                }else{
+                    env_true = env.putFalse(lit.getVariable());
+                    ImList<Clause> sub = new EmptyImList<Clause>();
+                    sub = substitute(clauses, new_lit);
+                    res = solve(sub, env_true);
+                }
+
+
+            }
+
+
+        }return res;
+
+
+
+
+
     }
 
     /**
      * given a clause list and literal, produce a new list resulting from
      * setting that literal to true
-     * 
+     *
      * @param clauses
      *            , a list of clauses
      * @param l
@@ -52,9 +133,19 @@ public class SATSolver {
      * @return a new list of clauses resulting from setting l to true
      */
     private static ImList<Clause> substitute(ImList<Clause> clauses,
-            Literal l) {
-        // TODO: implement this.
-        throw new RuntimeException("not yet implemented.");
+                                             Literal l) {
+        ImList<Clause> res = new EmptyImList<Clause>();
+        Clause clause = new Clause();
+
+        for (Clause c : clauses) {
+            clause = c.reduce(l);
+
+            if (clause != null){
+                res = res.add(clause);
+            }
+        }
+        return res;
     }
+
 
 }
--- SATSolver/src/main/java/sat/SATSolverTest.java
+++ SATSolver/src/main/java/sat/SATSolverTest.java
@@ -5,6 +5,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 */
+import com.sun.org.apache.xpath.internal.functions.FuncStartsWith;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
 import sat.env.*;
 import sat.formula.*;
@@ -19,31 +30,108 @@ public class SATSolverTest {
     Literal nc = c.getNegation();
 
 
+    public static void main(String[] args) throws IOException {
 
-	
-	// TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
-    
-	
+        /*
+        File f = null;
+        if (args.length > 0) {
+            f = new File(args[0]);
+        }
+        */
+
+
+        FileReader file = new FileReader("C:\\Users\\Jing Xiong\\AndroidStudioProjects\\Wk08\\code2d\\src\\main\\java\\sat\\sampleCNF\\s8Sat.cnf");
+        BufferedReader br = new BufferedReader(file);
+
+
+
+
+        BufferedWriter wr = new BufferedWriter(new FileWriter(
+                "C:\\Users\\Jing Xiong\\AndroidStudioProjects\\Wk08\\code2d\\src\\main\\java\\sat\\BoolAssignment.txt"));
+
+
+        String line;
+        String Clause = "";
+        Formula f2 = new Formula();
+        int numOfVar = 0;
+
+        while((line = br.readLine()) != null){
+            if(line.length() > 0) {
+                if (!(line.startsWith("p")) && !(line.startsWith("c"))) {
+                    line = line.trim();
+                    Clause c = new Clause();
+                    String[] s = line.split(" ");
+                    for (int i = 0; i < s.length - 1; i++) {
+                        if (s[i].contains("-")) {
+                            String next = s[i].substring(1, s[i].length());
+                            c = c.add(NegLiteral.make(next));
+                        } else {
+                            if (s[i] != " " && s[i] != null) {
+                                String next = s[i];
+                                c = c.add(PosLiteral.make(next));
+                            }
+                        }
+                    }
+                    f2 = f2.addClause(c);
+                }
+                else if (line.startsWith("p")) {
+                    String[] s = line.split(" ");
+                    numOfVar = Integer.parseInt(s[2]);
+                }
+            }
+        }
+
+        System.out.println("SAT solver starts!!!");
+        long started = System.nanoTime();
+        Environment res = SATSolver.solve(f2);
+        long time = System.nanoTime();
+        long timeTaken= time - started;
+        System.out.println("Time:" + timeTaken/1000000.0 + "ms");
+
+        if (res == null){
+            System.out.println("not satisfiable");
+        }else{
+            System.out.println("satisfiable");
+            for (int i = 1; i <= numOfVar; i++){
+                Bool ans = res.get(new Variable(Integer.toString(i)));
+                if (ans == Bool.TRUE){
+                    String num = Integer.toString(i);
+                    wr.write( num + ":" + "TRUE");
+                    wr.newLine();
+                }
+                else{
+                    String num = Integer.toString(i);
+                    wr.write( num + ":" + "FALSE");
+                    wr.newLine();
+                }
+            }
+            wr.close();
+        }
+    }
+
+
+
+
     public void testSATSolver1(){
-    	// (a v b)
-    	Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
+        // (a v b)
+        Environment e = SATSolver.solve(makeFm(makeCl(a,b))	);
 /*
     	assertTrue( "one of the literals should be set to true",
-    			Bool.TRUE == e.get(a.getVariable())  
+    			Bool.TRUE == e.get(a.getVariable())
     			|| Bool.TRUE == e.get(b.getVariable())	);
-    	
-*/    	
+
+*/
     }
-    
-    
+
+
     public void testSATSolver2(){
-    	// (~a)
-    	Environment e = SATSolver.solve(makeFm(makeCl(na)));
+        // (~a)
+        Environment e = SATSolver.solve(makeFm(makeCl(na)));
 /*
     	assertEquals( Bool.FALSE, e.get(na.getVariable()));
-*/    	
+*/
     }
-    
+
     private static Formula makeFm(Clause... e) {
         Formula f = new Formula();
         for (Clause c : e) {
@@ -51,7 +139,7 @@ public class SATSolverTest {
         }
         return f;
     }
-    
+
     private static Clause makeCl(Literal... e) {
         Clause c = new Clause();
         for (Literal l : e) {
@@ -59,7 +147,7 @@ public class SATSolverTest {
         }
         return c;
     }
-    
-    
-    
+
+
+
 }
